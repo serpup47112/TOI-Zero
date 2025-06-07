7a43e321a94e7d173c50bfb9953245c1ab86222b
Serxpp <nopnut2011@gmail.com>
2025-06-07 12:56:34 +0700

    Update A1-001.cpp

--- A1-001.cpp
+++ A1-001.cpp
@@ -3,19 +3,10 @@
 using namespace std;
 
 int main() {
-	char c = 'l', c1 = 'L';
-	string str;
-	cin >> str;
-	size_t n = str.find(c || c1);
-	if (n != string::npos) {
-        cout << "Found" << endl;
-    } else {
-        cout << "Not found" << endl;
-    }
-	/*string name, sur;
+	string name, sur;
 	cin >> name >> sur;
 	cout << "Hello" << " " << name << " " << sur << endl;
 	cout << name.substr(0 ,2) << sur.substr(0, 2);
 	
-	return 0;*/
+	return 0;
 }
